23fa9ca5e162aa6cd338b86b88b64c9ca0e21f4b
EllisBale <elscar29@gmail.com>
2026-05-16 03:17:03 +0100

    feat: add navbar html and css layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+const Navbar = () => {
+    return(
+        <nav className="absolute top-0 left-0 z-50 w-full after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 bg-[#222326] sm:bg-transparent backdrop-blur">
+  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div className="relative flex h-16 items-center justify-between">
+      <div className="relative inset-y-0 left-0 flex items-center justify-between sm:hidden">
+        {/* Mobile menu button */}
+        <button type="button" command="--toggle" commandfor="mobile-menu" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+          <span className="absolute -inset-0.5"></span>
+          <span className="sr-only">Open main menu</span>
+          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
+            <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6 not-in-aria-expanded:hidden">
+            <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+          </svg>
+        </button>
+      </div>
+      <div className="flex flex-1 items-center gap-5 justify-center sm:justify-start">
+        <div className="flex shrink-0 items-center">
+          <img src="/images/zonic_logo.png" alt="Zonic" className="h-25 w-auto object-contain" />
+        </div>
+        <div className="hidden sm:ml-6 sm:block text-[16px]">
+          <div className="flex space-x-4">
+            <a href="#" aria-current="page" className="rounded-md px-3 py-2 font-medium text-white">Home</a>
+            <a href="#" className="rounded-md px-3 py-2 font-medium text-white-300 hover:bg-white/5 hover:text-white">Phones</a>
+            <a href="#" className="rounded-md px-3 py-2 font-medium text-white-300 hover:bg-white/5 hover:text-white">Features</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <el-disclosure id="mobile-menu" hidden className="block sm:hidden">
+    <div class="space-y-1 px-2 pt-2 pb-3">
+      <a href="#" aria-current="page" className="block rounded-md px-3 py-2 text-base font-medium text-white">Dashboard</a>
+      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
+      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
+      <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+    </div>
+  </el-disclosure>
+</nav>
+    )
+};
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
 
 const Home = () => {
     return(
