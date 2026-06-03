f06e5d01841f68968673d0f8695331b0da14e142
EllisBale <elscar29@gmail.com>
2026-06-03 07:45:06 +0100

    feat: add buy button to phone cards

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 
 const Featured = () => {
 
@@ -41,35 +41,45 @@ const Featured = () => {
                     key={phone.id}
                     className="flex-shrink-0 w-3/4 sm:w-1/3 bg-zinc-800 rounded-lg shadow-md p-4 flex flex-col items-center snap-start"
                 >
-                    <img
-                    src={phone.images[0]}
-                    alt={phone.title}
-                    draggable="false"
-                    className="w-full h-100 sm:h-100 sm:w-100 object-cover rounded-md mb-4"
-                    />
+                    <Link to={`/phones/${phone.id}`}>
+                        <img
+                        src={phone.images[0]}
+                        alt={phone.title}
+                        draggable="false"
+                        className="w-full h-100 sm:h-100 sm:w-100 object-cover rounded-md mb-4"
+                        />
+                    </Link>
                     <h3 className="text-center text-lg font-medium">{phone.title}</h3>
-                    <p>Price: {phone.price}</p>
+                    <p className="mb-5">Price: {phone.price}</p>
+                    <Link to={`/phones/${phone.id}`} className="mt-auto block w-full text-white bg-gradient-to-br from-purple-600 rounded-full py-4 m to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-2 py-2.5 text-center leading-5">
+                        Buy
+                    </Link>
                 </li>
                 ))}
             </ul>
         </div>
 
-        <div className="hidden lg:flex mx-auto flex-col justify-center my-10">
-            <h2 className="text-center text-3xl font-semibold mb-6">Featured</h2>
-            <ul className="grid grid-cols-3 gap-6 w-full">
+        <div className="hidden lg:flex  flex-col justify-center mx-10 my-10">
+            <h2 className="text-center text-3xl font-semibold mb-8">Featured</h2>
+            <ul className="grid grid-cols-4 gap-8 w-full">
                 {FeaturedPhones.slice(0, 6).map((phone) => (
                 <li
                     key={phone.id}
                     className="bg-zinc-800 rounded-lg shadow-md p-4 flex flex-col items-center"
                 >
-                    <img
-                    src={phone.images[0]}
-                    alt={phone.title}
-                    draggable="false"
-                    className="w-full h-full object-cover rounded-md mb-4"
-                    />
+                    <Link to={`/phones/${phone.id}`}>
+                        <img
+                        src={phone.images[0]}
+                        alt={phone.title}
+                        draggable="false"
+                        className="w-full h-full object-cover rounded-md mb-4"
+                        />
+                    </Link>
                     <h3 className="text-center text-lg font-medium">{phone.title}</h3>
-                    <p>Price: {phone.price}</p>
+                    <p className="mb-5">Price: {phone.price}</p>
+                    <Link to={`/phones/${phone.id}`} className="mt-auto block w-full text-white bg-gradient-to-br from-purple-600 rounded-full py-4 m to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-2 py-2.5 text-center leading-5">
+                           Buy
+                    </Link>
                 </li>
                 ))}
             </ul>
--- src/pages/Phones.jsx
+++ src/pages/Phones.jsx
@@ -19,19 +19,23 @@ const Phones = () => {
     }, [] )
 
     return(
-        <div className="mt-[80px] mb-[30px] mx-5 lg:mx-50">
+        <div className="mt-[200px] mb-[30px] mx-2 lg:mx-50">
+            <h1 className="text-4xl font-bold text-shadow-white-950 mb-10">Explore Our Variety Of Smartphones</h1>
             <div className="grid grid-cols-12 gap-4 text-center">
                 {phones.map((phone) => (
-                    <div key={phone.id} className="col-span-6 md:col-span-4 xl:col-span-3 bg-zinc-800 rounded-lg shadow-md p-4 cursor-pointer">
+                    <div key={phone.id} className="flex flex-col col-span-6 md:col-span-4 xl:col-span-3 bg-zinc-800 rounded-lg shadow-md p-10">
                         <Link to={`/phones/${phone.id}`}>
                         <img
                             src={phone.images[0]}
                             alt={phone.title}
                             draggable="false"
-                            className="w-full h-100 sm:h-100 sm:w-100 object-cover rounded-md mb-4"
+                            className="w-full h-50 sm:h-100 sm:w-full object-cover rounded-md mb-4"
                         />
+                        </Link>
                         <h3 className="text-center text-lg font-medium">{phone.title}</h3>
-                        <p>£{phone.price}</p>
+                        <p className="mb-5">£{phone.price}</p>
+                        <Link to={`/phones/${phone.id}`} className="mt-auto block w-full text-white bg-gradient-to-br from-purple-600 rounded-full py-4 m to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-base text-sm px-2 py-2.5 text-center leading-5">
+                           Buy
                         </Link>
                     </div>
                 ))}
